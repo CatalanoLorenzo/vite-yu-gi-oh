2cedc03c68d5c8e3a5c7bc4989101238b6fcf9c5
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-17 20:52:19 +0200

    fix serch options

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -17,13 +17,16 @@ export default {
                 console.log(response.data.data);
                 console.log(response.data.data.type);
                 this.store.listCards = response.data.data
-                this.store.listCardsTypes = response.data.data.type
+                const arraytypes = response.data.data
+                arraytypes.forEach(element => {
+                    this.addtype(element.type)
+                }); 
                 })
         },
-        includes(){
-            this.store.listCards.forEach(e => {
-                this.store.listCardsTypes.push(e.type)
-            });
+        addtype(e){
+           if (!this.store.listCardsTypes.includes(e)){
+            this.store.listCardsTypes.push(e)
+           }
         }
 
 
@@ -39,19 +42,19 @@ export default {
     <main>
         <section class="search">
             <select name="" id="">
-                <option v-for="cards in this.store.listCards" value="" > <p v-if="!(this.store.listCardsTypes.includes(cards.type))">{{ cards.type }}</p></option>
+                <option v-for="types in this.store.listCardsTypes" :value="types" >{{types}}</option>
             </select>
         </section>
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-3 row-cols-lg-5">
                 <div class="col">
-                    <div class="card" v-for="card in this.store.listCards">
+                    <div class="card" v-for="card in this.store.listCards" >
                         <div class="card-header">
                             <img :src="card.card_images[0].image_url" alt="">
                         </div>
                         <div class="card-body">
                             <p>{{ card.name }}</p>
-                            <p>{{ card.type }}</p>
+                            <p v-if='addtype(card.type)'>{{ card.type }}</p>
                         </div>                        
                     </div>
                 </div>
--- src/store.js
+++ src/store.js
@@ -3,5 +3,5 @@ export const store = reactive({
     loading: true,
     urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=5&offset=5',
     listCards : null,
-    listCardsTypes : null
+    listCardsTypes : []
 })
