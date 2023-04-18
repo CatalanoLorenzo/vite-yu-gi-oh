3e78f78c6bf28af07a70813fc94b53ceb5cf5046
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-18 16:54:34 +0200

    add number of card and fix search

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -7,18 +7,32 @@ export default {
     data() {
         return {
             store,
-            typeSelect: '',
+            typeSelect: "",
         }
     },
     methods: {
+        listOfArcheType(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data);
+
+                });
+        },
         listCards(url) {
+            if (this.typeSelect != '') {
+                url += `&archetype=${this.typeSelect}`
+                console.log(this.store.urlApi);
+                // https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=5
+            }
             axios
                 .get(url)
                 .then(response => {
                     console.log(response.data.data);
-                    this.store.listCards = response.data.data
-                    this.store.listCards.forEach(element => {
-                        this.addtype(element.type)
+                    this.store.allListCards = response.data.data
+                    this.store.allListCards.forEach(element => {
+                        this.addtype(element.archetype
+                        )
                     });
                 })
         },
@@ -27,35 +41,18 @@ export default {
                 this.store.listCardsTypes.push(e)
             }
         },
-
-        /* searchForType() {
-            if (this.typeSelect != null) {
-                return this.store.urlApi + '&type=' + this.typeSelect
-            }
-            else {
-                return this.store.urlApi
-            }
-
-        } */
-        switchSelect(event) {
-      this.typeSelect = event.target.value;
     },
-        searchForType() {
-            if (this.typeSelect != '') {
-                return this.store.urlApi += '&type=' + this.typeSelect
-            }
-            else {
-                return this.store.urlApi
-            }
-    
+compued:{
+    numbercard(){
+        if (store.allListCards != null) {
+          return  store.allListCards.length
         }
-    },
-    computed:{
     }
-    ,
+},
     mounted() {
-        console.log(this.searchForType());
-        this.listCards(this.searchForType())
+        console.log(this.store.urlApi)
+        this.listCards(this.store.urlApi)
+        this.listOfArcheType(this.store.urlApiArcheType)
     }
 
 }
@@ -65,16 +62,21 @@ export default {
     <main>
 
         <section class="search">
-            <select name="" id=""   @change="switchSelect($event)">
-                <option v-for="types in this.store.listCardsTypes" :value="types">{{ types }}</option>
+            <select name="" id="" @change="listCards(store.urlApi)" v-model="typeSelect">
+                <option value="">Scegli il tipo di carta</option>
+                <option v-for="types in store.listCardsTypes" :value="types">{{ types }}</option>
             </select>
-            <div>{{ this.typeSelect }}</div>
+            <div>{{ typeSelect }}</div>
         </section>
-
+        <section>
+            <div>
+                <h3>{{ numbercard }}</h3>
+            </div>
+        </section>
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-3 row-cols-lg-5">
                 <div class="col">
-                    <div class="card" v-for="card in this.store.listCards">
+                    <div class="card" v-for="card in store.allListCards">
                         <div class="card-header">
                             <img :src="card.card_images[0].image_url" alt="">
                         </div>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { reactive } from "vue";
 export const store = reactive({
     loading: true,
-    urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=5',
-    listCards : null,
+    urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?',
+    urlApiArcheType : ' https://db.ygoprodeck.com/api/v7/archetypes.php',
+    allListCards : null,
     listCardsTypes : []
 })
