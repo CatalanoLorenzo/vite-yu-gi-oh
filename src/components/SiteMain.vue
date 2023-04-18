694e68858a92659bad005a70dd009c1eb540efbe
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-04-18 15:05:04 +0200

    add serch function

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -7,8 +7,7 @@ export default {
     data() {
         return {
             store,
-            typeSelect: '',
-            typeserch: []
+            typeSelect: null,
         }
     },
     methods: {
@@ -18,8 +17,7 @@ export default {
                 .then(response => {
                     console.log(response.data.data);
                     this.store.listCards = response.data.data
-                    const arraytypes = response.data.data
-                    arraytypes.forEach(element => {
+                    this.store.listCards.forEach(element => {
                         this.addtype(element.type)
                     });
                 })
@@ -30,26 +28,32 @@ export default {
             }
         },
 
-        
+        /* searchForType() {
+            if (this.typeSelect != null) {
+                return this.store.urlApi + '&type=' + this.typeSelect
+            }
+            else {
+                return this.store.urlApi
+            }
 
-    }
-    ,
+        } */
+
+    },
     computed:{
         searchForType() {
             if (this.typeSelect != null) {
-                this.store.listCards.forEach(e => {
-                if (this.typeSelect == e.type) {
-                    this.typeserch.push(e)
-                    console.log(this.typeserch);
-                }
-                console.log(this.typeserch);
-            })
+                return toString(this.store.urlApi += '&type=' + this.typeSelect)
             }
-            
+            else {
+                return this.store.urlApi
+            }
+
         }
-    },
+    }
+    ,
     mounted() {
-        this.listCards(this.store.urlApi)
+        console.log(searchForType);
+        this.listCards(searchForType)
     }
 
 }
@@ -57,26 +61,18 @@ export default {
 </script>
 <template>
     <main>
+
         <section class="search">
             <select name="" id="" v-model="this.typeSelect">
                 <option v-for="types in this.store.listCardsTypes" :value="types">{{ types }}</option>
             </select>
+            <div>{{ this.typeSelect }}</div>
         </section>
+
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-3 row-cols-lg-5">
-                <div v-if="this.typeSelect =! null" class="col">
+                <div class="col">
                     <div class="card" v-for="card in this.store.listCards">
-                        <div class="card-header">
-                            <img :src="card.card_images[0].image_url" alt="">
-                        </div>
-                        <div class="card-body">
-                            <p>{{ card.name }}</p>
-                            <p v-if='addtype(card.type)'>{{ card.type }}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col" v-else>
-                    <div class="card" v-for="card in this.typeserch">
                         <div class="card-header">
                             <img :src="card.card_images[0].image_url" alt="">
                         </div>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 export const store = reactive({
     loading: true,
-    urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=5&offset=5',
+    urlApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=5',
     listCards : null,
     listCardsTypes : []
 })
